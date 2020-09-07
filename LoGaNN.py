fefb3d972ab9fcd5c90d33f442d650f88320a98f
Noé Muñoz-Pérez <sightceo@gmail.com>
2020-09-07 10:38:08 -0500

    Update LoGaNN.py
    Compuerta lógica NOT

--- LoGaNN.py
+++ LoGaNN.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 4 15:27:37 2020
+Created on Mon Apr 20 15:27:37 2020
 
 @author: Noé Muñoz Pérez
 """
@@ -11,6 +11,7 @@ from keras.layers.core import Dense
 
 # cargamos las 4 combinaciones de las compuertas lógicas
 training_data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
+# not_data = np.array([0,1], "float32")
 
 # y estos son los resultados que se obtienen de la compuerta XOR, en el mismo orden
 XOR_target = np.array([[0],[1],[1],[0]], "float32")
@@ -24,6 +25,8 @@ AND_target = np.array([[0],[0],[0],[1]], "float32")
 NAND_target = np.array([[1],[1],[1],[0]], "float32")
 # y estos son los resultados que se obtienen de la compuerta IFTHEN, en el mismo orden
 IFTHEN_target = np.array([[1],[1],[0],[1]], "float32")
+# y estos son los resultados que se obtienen de la compuerta NOT, en el mismo orden
+NOT_target = np.array([[1,1],[1,0],[0,1],[0,0]], "float32")
 
 model = Sequential()
 model.add(Dense(16, input_dim=2, activation='relu'))
@@ -32,11 +35,17 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
 
 while(1):
- print("Logic gates neural networks\n1. AND\n2. OR\n3. Implicación\n4. XOR\n5. NOR\n6. NAND\nE. Exit\nWhich model do you choose? ")
+ print("Logic gates neural networks\n1. NOT\n2. AND\n3. OR\n4. Implicación\n5. XOR\n6. NOR\n7. NAND\nE. Exit\nWhich model do you choose? ")
  answer = input()
  if(answer == 'E'):
   break
  elif(int(answer) == 1):
+  # se usa el mismo vector de entrenamiento para que el tamaño del vector de entrenamiento coincida con el tamaño esperado por los parámetros decompilación
+  model.fit(training_data, NOT_target, epochs=50)
+  scores = model.evaluate(training_data, AND_target)
+  print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+  print (model.predict(training_data).round())
+ elif(int(answer) == 2):
 
   model.fit(training_data, AND_target, epochs=110)
 
@@ -45,7 +54,7 @@ while(1):
 
   print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
   print (model.predict(training_data).round())
- elif(int(answer) == 2):
+ elif(int(answer) == 3):
 
   model.fit(training_data, OR_target, epochs=210)
 
@@ -54,13 +63,13 @@ while(1):
 
   print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
   print (model.predict(training_data).round())
- elif(int(answer) == 3):
+ elif(int(answer) == 4):
   model.fit(training_data, IFTHEN_target, epochs=210)
   # evaluamos el modelo
   scores = model.evaluate(training_data, IFTHEN_target)
   print("\%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
   print (model.predict(training_data).round())
- elif(int(answer) == 4):
+ elif(int(answer) == 5):
 
   model.fit(training_data, XOR_target, epochs=200)
 
@@ -69,7 +78,7 @@ while(1):
 
   print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
   print (model.predict(training_data).round())
- elif(int(answer) == 5):
+ elif(int(answer) == 6):
 
   model.fit(training_data, NOR_target, epochs=200)
 
@@ -78,7 +87,7 @@ while(1):
 
   print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
   print (model.predict(training_data).round())
- elif(int(answer) == 6):
+ elif(int(answer) == 7):
 
   model.fit(training_data, NAND_target, epochs=200)
 
